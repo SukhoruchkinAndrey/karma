adb2a60aca31566a451e490ce0bb5465b9b0ce25
ps.borisov <ps.borisov@tensor.ru>
2018-05-19 18:17:00 +0300

    odnoi rukoi za 30 seconds

--- src/components/ListContainer/ListContainer.js
+++ src/components/ListContainer/ListContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import List from '../List/List';
-
+import './ListContainer.css';
 const sortItems = (items, sorting, columnName) => {
    const newItems = items.sort(
       (firstItem, secondItem) =>
@@ -8,7 +8,11 @@ const sortItems = (items, sorting, columnName) => {
             ? firstItem[columnName] - secondItem[columnName]
             : secondItem[columnName] - firstItem[columnName]
    );
-   return Promise.resolve(newItems);
+   return new Promise(resolve => {
+      setTimeout(() => {
+         resolve(newItems);
+      }, 1000);
+   });
 };
 
 class ListContainer extends Component {
@@ -25,27 +29,32 @@ class ListContainer extends Component {
    handleColumnClick = columnName => {
       const { items, currentSort, currentSortedColumn } = this.state;
       const newSort = currentSortedColumn === columnName ? !currentSort : true;
+      this.setState({     isLoading: true      });
       sortItems(items, newSort, columnName).then(newItems => {
          this.setState(prevState => ({
             items: newItems,
             currentSort: newSort,
-            currentSortedColumn: columnName
+            currentSortedColumn: columnName,
+            isLoading: false
          }));
       });
    };
 
    render() {
-      const { items, currentSort, currentSortedColumn } = this.state;
+      const { items, currentSort, currentSortedColumn, isLoading } = this.state;
       const { columns } = this.props;
 
       return (
-         <List
-            items={items}
-            columns={columns}
-            currentSort={currentSort}
-            currentSortedColumn={currentSortedColumn}
-            handleColumnClick={this.handleColumnClick}
-         />
+         <div className="ListContainer">
+            {isLoading && <div className="ListContainer__loadingIndicator"></div>}
+            <List
+               items={items}
+               columns={columns}
+               currentSort={currentSort}
+               currentSortedColumn={currentSortedColumn}
+               handleColumnClick={this.handleColumnClick}
+            />
+         </div>
       );
    }
 }
