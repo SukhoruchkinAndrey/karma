fba584c2fdd60866f9f1484f04c48df08f73043b
Aleksandr Zaitsev <alserz1@gmail.com>
2018-05-19 20:43:19 +0300

    fragment instead of div

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from '../../logo.jpg';
 
 const Layout = ({ children }) => (
-   <div className="App">
+   <Fragment>
       <header className="App-header">
          <img src={logo} className="App-logo" alt="logo" />
          <h1 className="App-title">Bibosik</h1>
       </header>
       {children}
       <footer>Доволен?</footer>
-   </div>
+   </Fragment>
 );
 
 export default Layout;
