437dda198378528c0f0eb20983e06768d15570b7
as.zaycev <as.zaycev@tensor.ru>
2018-05-22 12:18:26 +0300

    code-splitting

--- src/components/Application/Application.js
+++ src/components/Application/Application.js
@@ -1,16 +1,13 @@
 //@flow
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MainInfo from '../MainInfo/MainInfo';
-import PersonInfo from '../PersonInfo/PersonInfo';
-import FourZeroFour from '../FourZeroFour/FourZeroFour';
 import Layout from '../Layout/Layout';
-import AddComment from '../AddComment/AddComment';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import ApolloClient from 'apollo-boost';
-import CommentInfo from '../CommentInfo/CommentInfo';
+import Loadable from 'react-loadable';
+import Loading from '../Loading/Loading';
 
 const httpLink = createHttpLink({
    uri: 'https://api.graph.cool/simple/v1/cjheqf6vg01lo0196ev30m9zb'
@@ -36,20 +33,49 @@ const client = new ApolloClient({
    uri: 'https://api.graph.cool/simple/v1/cjheqf6vg01lo0196ev30m9zb'
 });
 
+const LoadableMainInfo = Loadable({
+   loader: () => import('../MainInfo/MainInfo'),
+   loading: Loading
+});
+
+const LoadableAddComment = Loadable({
+   loader: () => import('../AddComment/AddComment'),
+   loading: Loading
+});
+
+const LoadablePersonInfo = Loadable({
+   loader: () => import('../PersonInfo/PersonInfo'),
+   loading: Loading
+});
+
+const LoadableCommentInfo = Loadable({
+   loader: () => import('../CommentInfo/CommentInfo'),
+   loading: Loading
+});
+
+const LoadableFourZeroFour = Loadable({
+   loader: () => import('../FourZeroFour/FourZeroFour'),
+   loading: Loading
+});
+
 const Application = () => (
    <BrowserRouter>
       <ApolloProvider client={client}>
          <Layout>
             <Switch>
-               <Route path="/" component={MainInfo} exact />
-               <Route path="/addComment" component={AddComment} exact />
-               <Route path="/person/:userId" component={PersonInfo} exact />
+               <Route path="/" component={LoadableMainInfo} exact />
+               <Route path="/addComment" component={LoadableAddComment} exact />
                <Route
-                  path="/comment/:commentId"
-                  component={CommentInfo}
+                  path="/person/:userId"
+                  component={LoadablePersonInfo}
                   exact
                />
-               <Route component={FourZeroFour} />
+               <Route
+                  path="/comment/:commentId"
+                  component={LoadableCommentInfo}
+                  exact
+               />
+               <Route component={LoadableFourZeroFour} />
             </Switch>
          </Layout>
       </ApolloProvider>
